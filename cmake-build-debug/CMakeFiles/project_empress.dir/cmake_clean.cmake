fdde9ac964f544ef7ae0d41068f92facae300b8e
Milena Michalska <milena@dige.pl>
2017-03-19 22:33:29 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/project_empress.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_empress.dir/main.c.obj"
+  "project_empress.pdb"
+  "project_empress.exe"
+  "project_empress.exe.manifest"
+  "libproject_empress.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/project_empress.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
